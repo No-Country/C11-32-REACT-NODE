4ed337589bb3772d735360f873c85ef6d7ecaf92
Paul <moicoxoxo@gmail.com>
2023-05-24 17:35:04 -0400

    change swr to tanstack-query

--- backend/src/controllers/suscriptions.controller.ts
+++ backend/src/controllers/suscriptions.controller.ts
@@ -1,18 +1,11 @@
 import { NextFunction, Request, Response } from "express";
-import Stripe from "stripe";
-import { CONFIG } from "~/config/config";
+import { stripe } from "~/libs";
 
 export function newSuscription(req: Request, res: Response, next: NextFunction) {
   try {
-    const stripe = new Stripe(CONFIG.STRIPE_SECRET_KEY, { apiVersion: "2022-11-15", typescript: true });
+    req.body;
 
-    // stripe.prices.list({ expand: ["data.product"] }).then((products) => {
-    //   res.send(products);
-    // });
-
-    stripe.plans.list({ expand: ["data.product"] }).then((plans) => {
-      return res.json(plans);
-    });
+    stripe.paymentMethods.create({});
   } catch (error) {
     next(error);
   }
