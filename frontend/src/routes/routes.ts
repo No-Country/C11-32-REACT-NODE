40356732abd3502d20c01d00fa7c35318e6e4c85
Paul <moicoxoxo@gmail.com>
2023-05-24 17:57:13 -0400

    add constants routes

---
+++ frontend/src/routes/routes.ts
@@ -0,0 +1,18 @@
+export const ROUTES = {
+  main: "/",
+  about: "/about",
+  roadMap: "/roadmap",
+  blog: "/blog",
+  rooms: "/rooms",
+  auth: {
+    login: "/auth/login",
+    register: "/auth/register",
+    logout: "/auth/logout",
+  },
+  suscriptions: {
+    plans: "/pricing",
+    payment: "/suscriptions/payment",
+    success: "/sucriptions/success",
+    cancel: "/sucriptions/cancel",
+  },
+};
