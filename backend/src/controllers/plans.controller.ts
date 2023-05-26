7908911035f2648de00aeaff7d706dae047ed4ca
Paul <moicoxoxo@gmail.com>
2023-05-26 09:52:52 -0400

    fix response plan

--- backend/src/controllers/plans.controller.ts
+++ backend/src/controllers/plans.controller.ts
@@ -4,7 +4,7 @@ import { stripe } from "~/libs";
 export function getPlans(req: Request, res: Response, next: NextFunction) {
   try {
     stripe.plans.list({ expand: ["data.product"] }).then((plans) => {
-      return res.json(plans);
+      return res.json(plans.data);
     });
   } catch (error) {
     next(error);
