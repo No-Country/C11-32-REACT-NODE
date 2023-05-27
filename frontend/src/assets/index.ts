a671c310fcc8a5b63775bcaa7b2553da78b996a5
Paul <moicoxoxo@gmail.com>
2023-05-26 20:44:45 -0400

    add spin asset

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -2,5 +2,6 @@ import logo from "./logo.svg";
 import CheckedIc from "./CheckedIc";
 import CardIc from "./CardIc";
 import TaskFinishIc from "./TaskFinishIc";
+import Spin from "./Spin";
 
-export { logo, CheckedIc, CardIc, TaskFinishIc };
+export { logo, CheckedIc, CardIc, TaskFinishIc, Spin };
